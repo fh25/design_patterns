a9fba9b9001a6f2d10d8d5df32aa0010c950038f
Fernando <fherrera000@gmail.com>
2019-03-28 23:06:54 -0500

    Make thread safe

--- CreationalPatterns/src/main/java/com/creational/patterns/singleton/DbSingleton.java
+++ CreationalPatterns/src/main/java/com/creational/patterns/singleton/DbSingleton.java
@@ -1,14 +1,26 @@
 package com.creational.patterns.singleton;
 
 public class DbSingleton {
-  private static DbSingleton instance = null;
+//  volatile keyword used to ensure that singleton remains throughout JVM
+  private static volatile DbSingleton instance = null;
 
-  // private so it can't be newed up
-  private DbSingleton() {}
+  // private so it can't be newed up and protect against Reflection
+  private DbSingleton() {
+    if(instance != null) {
+      throw new RuntimeException("Use getInstance() to create");
+    }
+  }
 
+//  Make thread safe
+//  If 2 threads come in, synchronize...don't sync method since that'd
+//  be a performance hit. Sync inside method with another null check
   public static DbSingleton getInstance() {
     if(instance == null) {
-      instance = new DbSingleton();
+      synchronized (DbSingleton.class) {
+        if(instance == null) {
+          instance = new DbSingleton();
+        }
+      }
     }
 
     return instance;
