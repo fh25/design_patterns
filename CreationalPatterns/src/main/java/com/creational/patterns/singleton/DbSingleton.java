a0a562abec91e223d7a4d2f2ab092e2a82aed582
Fernando <fherrera000@gmail.com>
2019-03-28 22:18:06 -0500

    lazily loading singleton instance with null check

--- CreationalPatterns/src/main/java/com/creational/patterns/singleton/DbSingleton.java
+++ CreationalPatterns/src/main/java/com/creational/patterns/singleton/DbSingleton.java
@@ -1,12 +1,16 @@
 package com.creational.patterns.singleton;
 
 public class DbSingleton {
-  private static DbSingleton instance = new DbSingleton();
+  private static DbSingleton instance = null;
 
   // private so it can't be newed up
   private DbSingleton() {}
 
   public static DbSingleton getInstance() {
+    if(instance == null) {
+      instance = new DbSingleton();
+    }
+
     return instance;
   }
 
